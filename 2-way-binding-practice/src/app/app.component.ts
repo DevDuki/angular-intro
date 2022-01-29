bbd9b969db16c500760951fb9355523e06bc964d
Dusan Misic <dmisic@inventage.com>
2022-01-29 18:53:17 +0100

    Udemy Basics 2-way-bindings

---
+++ 2-way-binding-practice/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username: string = '';
+  isEmpty: boolean = this.username === '';
+
+  updateUserName(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value
+  }
+
+  resetUserName() {
+    this.username = ''
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# angular-intro
-angular intro from https://www.youtube.com/watch?v=3dHNOWTI7H8
+# angular-courses
+
+##Tasks-Manager
+from https://www.youtube.com/watch?v=3dHNOWTI7H8
+
+##Recipe-collection
+from https://www.udemy.com/share/101WAU3@2IfvqgmtH05Q4pRyLL4_FkY2759p4EslEClNLQJymvGcf1mk9U09pD8CTQT63Jbs/
