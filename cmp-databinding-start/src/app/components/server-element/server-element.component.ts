15c3bbe0e7a5f7c8ddf7b169b7aabadabf0a6b0e
DevDuki <dusan-misic@hotmail.com>
2022-02-04 20:02:53 +0100

    attribute/event binding

--- cmp-databinding-start/src/app/components/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/components/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  OnDestroy, ViewChild, ElementRef, ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,16 +19,55 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, OnDestroy {
   @Input('srvElement') element!: {
     type: string,
     name: string,
     content: string
   };
+  @Input() name!: string;
+  @ViewChild('heading', { static: true }) header!: ElementRef;
+  @ContentChild('contentParagraph') paragraph!: ElementRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called!')
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!')
+    console.log('changes in OnChanges', changes)
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!')
+    console.log('Is this DOM element available in this lifecycle yet?', this.header.nativeElement.textContent);
+    console.log('Text Content of contentParagraph', this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!')
+    console.log('Is this DOM element available in this lifecycle yet?', this.header.nativeElement.textContent);
+    console.log('Text Content of contentParagraph', this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!')
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!')
+    console.log('Is this DOM element available in this lifecycle yet?', this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!')
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!')
+  }
 }
