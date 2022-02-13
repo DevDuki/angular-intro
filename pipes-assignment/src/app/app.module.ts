b2073e1bdbfe8f391a7d2ef830e8c29a028c3531
Dusan Misic <dmisic@inventage.com>
2022-02-13 10:01:19 +0100

    pipes assignment

---
+++ pipes-assignment/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReversePipe,
+    SortPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
